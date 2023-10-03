9d633909fc99492475aa46b7596327af69002f7c
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-10-03 13:03:36 +0300

    products list

--- task2/src/ShoppingCart.jsx
+++ task2/src/ShoppingCart.jsx
@@ -27,7 +27,7 @@ class ShoppingCart extends Component {
     const count = this.state.cartItems.length;
     return (
       <div className="column">
-        <CartTitle userName={this.props.userData.firstName} count={count} />
+        <CartTitle count={count} />
         <ProductsList cartItems={this.state.cartItems} />
       </div>
     );
